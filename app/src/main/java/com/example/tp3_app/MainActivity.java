8f0281948b302aaa87706b05cad10831ea8b40ca
Christian-Mb <44323151+Christian-Mb@users.noreply.github.com>
2020-02-05 14:17:20 +0100

    fix bug

--- app/src/main/java/com/example/tp3_app/MainActivity.java
+++ app/src/main/java/com/example/tp3_app/MainActivity.java
@@ -19,123 +19,25 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> planetes;
+
     ListView listview;
     PlaneteAdapter adapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        installePlanetes();
+
         listview = (ListView) findViewById(R.id.listView);
-        adapter = new PlaneteAdapter();
+        adapter = new PlaneteAdapter(MainActivity.this.getLayoutInflater(),MainActivity.this.getApplicationContext());
         listview.setAdapter(adapter);
 
     }
 
-    private void installePlanetes() {
-        planetes = new ArrayList<String>();
-        planetes.add("Mercure");
-        planetes.add("Venus");
-        planetes.add("Terre");
-        planetes.add("Mars");
-        planetes.add("Jupiter");
-        planetes.add("Saturne");
-        planetes.add("Uranus");
-        planetes.add("Neptune");
-        planetes.add("Pluton");
-    }
 
 
-    class PlaneteAdapter extends BaseAdapter {
 
 
-        /**
-         * How many items are in the data set represented by this Adapter.
-         *
-         * @return Count of items.
-         */
-        @Override
-        public int getCount() {
-            return  planetes.size();
-
-        }
-
-        /**
-         * Get the data item associated with the specified position in the data set.
-         *
-         * @param position Position of the item whose data we want within the adapter's
-         *                 data set.
-         * @return The data at the specified position.
-         */
-        @Override
-        public Object getItem(int position) {
-            return planetes.get(position);
-        }
-
-        /**
-         * Get the row id associated with the specified position in the list.
-         *
-         * @param position The position of the item within the adapter's data set whose row id we want.
-         * @return The id of the item at the specified position.
-         */
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        /**
-         * Get a View that displays the data at the specified position in the data set. You can either
-         * create a View manually or inflate it from an XML layout file. When the View is inflated, the
-         * parent View (GridView, ListView...) will apply default layout parameters unless you use
-         * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
-         * to specify a root view and to prevent attachment to the root.
-         *
-         * @param position    The position of the item within the adapter's data set of the item whose view
-         *                    we want.
-         * @param convertView The old view to reuse, if possible. Note: You should check that this view
-         *                    is non-null and of an appropriate type before using. If it is not possible to convert
-         *                    this view to display the correct data, this method can create a new view.
-         *                    Heterogeneous lists can specify their number of view types, so that this View is
-         *                    always of the right type (see {@link #getViewTypeCount()} and
-         *                    {@link #getItemViewType(int)}).
-         * @param parent      The parent that this view will eventually be attached to
-         * @return A View corresponding to the data at the specified position.
-         */
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View itemView = convertView;
-            if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater)    MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                itemView = inflater.inflate(R.layout.listitem, null);
-            }
-
-
-            TextView nomPlanete = (TextView) itemView.findViewById(R.id.textView);
-            final CheckBox checkBox = (CheckBox) itemView.findViewById(R.id.checkbox);
-            final Spinner spinner = (Spinner) itemView.findViewById(R.id.spinner);
-
-            nomPlanete.setText(planetes.get(position));
-
-            String[] taillePlanetes = {"4900", "12000", "12800", "6800", "144000", "120000", "52000", "50000", "2300"};
-            final ArrayAdapter<String> spinadapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, taillePlanetes);
-            spinadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            spinner.setAdapter(spinadapter);
-
-            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                    CheckBox checkBox = (CheckBox) compoundButton.findViewById(R.id.checkbox);
-                    spinner.setEnabled(!checkBox.isChecked());
-                    spinadapter.notifyDataSetChanged();
-                }
-            });
-
-            return itemView;
-        }
-    }
 
 }
 
---
+++ app/src/main/java/com/example/tp3_app/PlaneteAdapter.java
@@ -0,0 +1,125 @@
+package com.example.tp3_app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+class PlaneteAdapter extends BaseAdapter {
+
+    private ArrayList<String> planetes;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+
+
+
+    public PlaneteAdapter(LayoutInflater layoutInflater, Context context) {
+        this.layoutInflater = layoutInflater;
+        this.context= context;
+            installePlanetes();
+    }
+    private void installePlanetes() {
+        planetes = new ArrayList<String>();
+        planetes.add("Mercure");
+        planetes.add("Venus");
+        planetes.add("Terre");
+        planetes.add("Mars");
+        planetes.add("Jupiter");
+        planetes.add("Saturne");
+        planetes.add("Uranus");
+        planetes.add("Neptune");
+        planetes.add("Pluton");
+    }
+
+    /**
+     * How many items are in the data set represented by this Adapter.
+     *
+     * @return Count of items.
+     */
+    @Override
+    public int getCount() {
+        return  planetes.size();
+
+    }
+
+    /**
+     * Get the data item associated with the specified position in the data set.
+     *
+     * @param position Position of the item whose data we want within the adapter's
+     *                 data set.
+     * @return The data at the specified position.
+     */
+    @Override
+    public Object getItem(int position) {
+        return planetes.get(position);
+    }
+
+    /**
+     * Get the row id associated with the specified position in the list.
+     *
+     * @param position The position of the item within the adapter's data set whose row id we want.
+     * @return The id of the item at the specified position.
+     */
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    /**
+     * Get a View that displays the data at the specified position in the data set. You can either
+     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+     * parent View (GridView, ListView...) will apply default layout parameters unless you use
+     * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
+     * to specify a root view and to prevent attachment to the root.
+     *
+     * @param position    The position of the item within the adapter's data set of the item whose view
+     *                    we want.
+     * @param convertView The old view to reuse, if possible. Note: You should check that this view
+     *                    is non-null and of an appropriate type before using. If it is not possible to convert
+     *                    this view to display the correct data, this method can create a new view.
+     *                    Heterogeneous lists can specify their number of view types, so that this View is
+     *                    always of the right type (see {@link #getViewTypeCount()} and
+     *                    {@link #getItemViewType(int)}).
+     * @param parent      The parent that this view will eventually be attached to
+     * @return A View corresponding to the data at the specified position.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View itemView = convertView;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater)   this.layoutInflater;
+            itemView = inflater.inflate(R.layout.listitem, null);
+        }
+
+
+        TextView nomPlanete = (TextView) itemView.findViewById(R.id.textView);
+        final CheckBox checkBox = (CheckBox) itemView.findViewById(R.id.checkbox);
+        final Spinner spinner = (Spinner) itemView.findViewById(R.id.spinner);
+
+        nomPlanete.setText(planetes.get(position));
+
+        String[] taillePlanetes = {"4900", "12000", "12800", "6800", "144000", "120000", "52000", "50000", "2300"};
+        final ArrayAdapter<String> spinadapter = new ArrayAdapter<String>(this.context, android.R.layout.simple_spinner_item, taillePlanetes);
+        spinadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinadapter);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                CheckBox checkBox = (CheckBox) compoundButton.findViewById(R.id.checkbox);
+                spinner.setEnabled(!checkBox.isChecked());
+                spinadapter.notifyDataSetChanged();
+            }
+        });
+
+        return itemView;
+    }
+}
