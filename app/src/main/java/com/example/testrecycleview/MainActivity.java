6dd7710b5595c697ed6d5aa158bdb28668e42d05
Christian-Mb <44323151+Christian-Mb@users.noreply.github.com>
2020-03-11 15:04:21 +0100

    end

--- app/src/main/java/com/example/testrecycleview/MainActivity.java
+++ app/src/main/java/com/example/testrecycleview/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -30,7 +31,18 @@ public class MainActivity extends AppCompatActivity  implements DetecteurDeClicS
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mcoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
         mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
+
+        mLayoutManager=new GridLayoutManager(this,2,GridLayoutManager.VERTICAL, false);
+
+        ((GridLayoutManager)mLayoutManager) .setSpanSizeLookup(
+                new GridLayoutManager.SpanSizeLookup() {
+
+                    @Override
+                    public int getSpanSize(int arg0) {
+                        return (arg0 % 3) == 0 ? 2 : 1;
+                    }
+                });
+
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new MonRecyclerViewAdapter(getDataSource());
         mRecyclerView.setAdapter(mAdapter);
