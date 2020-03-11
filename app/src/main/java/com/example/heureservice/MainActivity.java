4bbd5572a2a037c06b0c1bf3884ab4eb293d327f
Christian-Mb <44323151+Christian-Mb@users.noreply.github.com>
2020-03-11 15:37:16 +0100

    exo 1

--- app/src/main/java/com/example/heureservice/MainActivity.java
+++ app/src/main/java/com/example/heureservice/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.heureservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private int n =0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button serviceBtn = (Button) findViewById(R.id.serviceBtn);
+
+
+        serviceBtn.setOnClickListener( new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, HeureService.class);
+
+                if (n ==0){
+                    serviceBtn.setText("Arrêter le service");
+                    startService(intent);
+                    n++;
+
+                }
+                else{
+                    serviceBtn.setText("Quelle heure?");
+
+                    stopService(intent);
+                    n--;
+                }
+            }
+        });
     }
+
 }
